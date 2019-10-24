6d81bbe726f2a6cf36b4277e1c26676d6e0641c3
srfoster <stephen@thoughtstem.com>
2019-10-24 10:07:41 -0700

    fix dep

--- metacoders-dot-org-dc-lib/info.rkt
+++ metacoders-dot-org-dc-lib/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection    "metacoders-dot-org-dc-lib")
-(define deps '("base" "metacoders-dot-org-lib"))
+(define deps '("base" 
+               "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
