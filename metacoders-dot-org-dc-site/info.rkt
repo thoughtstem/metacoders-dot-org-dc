7c411158fa4bbc1666d4d2ac1abc48ab2f970c2e
srfoster <stephen@thoughtstem.com>
2019-10-24 10:21:12 -0700

    messing with info files

--- metacoders-dot-org-dc-site/info.rkt
+++ metacoders-dot-org-dc-site/info.rkt
@@ -1,3 +1,4 @@
 #lang info
 (define collection    "metacoders-dot-org-dc-site")
-(define deps '("base" "../metacoders-dot-org-dc-lib"))
+(define deps '("base" 
+               "https://github.com/thoughtstem/metacoders-dot-org-dc.git?path=metacoders-dot-org-dc-site"))
