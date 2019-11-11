2de2e3234c09fb0e2b44fe5287e7e6bf088a4519
sonnynajar <sonny.najar@gmail.com>
2019-11-11 10:05:13 -0800

    Update main.rkt

--- metacoders-dot-org-dc-site/main.rkt
+++ metacoders-dot-org-dc-site/main.rkt
@@ -2,53 +2,34 @@
 
 (provide pages index)
 
-(require metacoders-dot-org-dc-lib)
+(require metacoders-dot-org-dc-lib
+         metacoders-dot-org-lib)
 
-(define (buy-button price)
-  ;<!-- Load Stripe.js on your website. -->
-  ;(script src:"https://js.stripe.com/v3")
-
-  ;<!-- Create a button that your customers click to complete their purchase. Customize the styling to suit your branding. -->
-  (list
-   (button-primary
-    style:"background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
-    id:"checkout-button-sku_G7REBMxlyd6Oh1"
-    ;role:"link"
-    (~a "Buy Now for $" price))
-   
-   (div id:"error-message")))
-
-#|<script>
-(function() {
+(define (buy-button price sku)
+  (list (button-secondary id:(~a "checkout-button-" sku)
+                          (~a "Enroll Now for $" price))
+        (div id:(~a "error-message" sku))
+        (script src:"https://js.stripe.com/v3")
+        @script/inline{
+ (function() {
   var stripe = Stripe('pk_test_BZvU77rH9zfNQvab1EpKB7GK00ZxANulPE');
 
-  var checkoutButton = document.getElementById('checkout-button-sku_G7REBMxlyd6Oh1');
+  var checkoutButton = document.getElementById('checkout-button-@sku');
   checkoutButton.addEventListener('click', function () {
-    ; When the customer clicks on the button, redirect
-    ; them to Checkout.
-    stripe.redirectToCheckout({
-      items: [{sku: 'sku_G7REBMxlyd6Oh1', quantity: 1}],
 
-      ; Do not rely on the redirect to the successUrl for fulfilling
-      ; purchases, customers may not always reach the success_url after
-      ; a successful payment.
-      ; Instead use one of the strategies described in
-      ; https://stripe.com/docs/payments/checkout/fulfillment
+    stripe.redirectToCheckout({
+      items: [{sku: '@sku', quantity: 1}],
       successUrl: 'https://your-website.com/success',
       cancelUrl: 'https://your-website.com/canceled',
     })
     .then(function (result) {
       if (result.error) {
-        ; If `redirectToCheckout` fails due to a browser or network
-        ; error, display the localized error message to your customer.
-        var displayError = document.getElementById('error-message');
+        var displayError = document.getElementById('error-message@sku');
         displayError.textContent = result.error.message;
       }
     });
   });
-})();
-</script>
-|#
+})();}))
 
 (define (print-dates dates [s ""])   
   (if (> (length dates) 1)
@@ -68,7 +49,8 @@
          #:end-time      [end-time "TBA"]
          #:location      [location "TBA"]
          #:address       [address "TBA"]
-         #:price         [price "TBA"])
+         #:price         [price "TBA"]
+         #:sku           [sku   ""])
   (card
    style: (properties
            margin: 10
@@ -78,7 +60,7 @@
        title)
    (card-body
     (h3 topic)
-    (h4 age-range)
+    (h5 (strong "Grades: " age-range))
     (h5 (strong "Start Date: ") (first meeting-dates) " @ " start-time)
     (card-img-top
      src: image-url
@@ -89,13 +71,35 @@
     (p (strong "Times:") " from " start-time " to " end-time ".")
     (p (strong "Dates: ") (print-dates meeting-dates))
     (p (strong "Location: ") location " - " address ".")
-    (buy-button price))))
+    (buy-button price sku))))
 
-(define (courses)
-  (list
-   (row
-    (col-5
-     (course-card
+(define (location-courses name course-1 [course-2 (p)])
+  (div
+   (div
+    style: (properties
+            text-align: "center")
+    (br)
+    (h1 (~a "Sign up for courses at " name "!"))
+    (br))
+            
+   (div
+    style: (properties
+            position: "absolute"
+            left: "50%"
+            margin-left: -600)
+    (row
+     (col-5
+      course-1)
+     (col-5
+      style:
+      (properties
+       margin-left: 50)
+      course-2)))
+   (div style: (properties height: 1000))
+  ))
+
+(define (weekend-course)
+  (course-card
       #:title         "WEEKEND CLASS"
       #:topic         "Point & Click Games!"
       #:description   "In our Winter program, students at La Jolla will learn how to code their own whack-a-mole style games that utilize the mouse. Students will learn how to customize their game with near endless possible combinations: Are you a UFO collecting cows? Or a kid eating up their favorite foods and avoiding their least favorites? Or a ninja nabbing fruit and not the bombs? Their games will feature a point system, and students will code in-game items such that they gain or lose points when the player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!"
@@ -106,12 +110,11 @@
       #:end-time      "12:30 pm"
       #:location      "Gallaudet University"
       #:address       "800 Florida Ave NE, Washington, DC 20002"
-      #:price         185))
-    (col-5
-     style:
-     (properties
-      margin-left: 50)
-     (course-card
+      #:price         185
+      #:sku           "sku_G7REBMxlyd6Oh1"))
+
+(define (summer-camp)
+  (course-card
       #:title         "SUMMER CAMP"
       #:topic         "Exploring 3D Worlds!"
       #:description   "The magic of turning code into an immersive 3D world is something that only coders will ever experience. In our Winter program, students at La Jolla Elementary will learn a powerful programming language for designing and customizing beautiful interactive worlds that they can walk (or fly!) through. Beginners will be surprised to find that with just a few lines of code, they are able to create places that have never existed before. Advanced students will be pleasantly surprised at how deep and subtle the art of world-building really is. In this session, students will continue to earn badges as they become more and more fluent in coding."
@@ -122,19 +125,10 @@
       #:end-time      "1:00 pm"
       #:location      "Gallaudet University"
       #:address       "800 Florida Ave NE, Washington, DC 20002"
-      #:price         325)))))
-   
-(define (demo-thing . content)
-  (div 
-    style: (properties background-color: "red")
-    content))
+      #:price         325
+      #:sku           "sku_GA18hKlhrjqjfj"))
 
-(define (point-of-sale)
-  (list
-    (h1 "HI, buy stuff!")
-    (button-primary "Buy")
-    (demo-thing
-      (h2 "I am a div"))))
+
 
 (define (index)
   (page index.html
@@ -148,26 +142,27 @@
                    ;Gross...
                    (pathify
                      (add-path-prefix logo-img-path)))))
+          
+          (list
+           (location-courses
+           "Gallaudet"
+           (weekend-course)
+           (summer-camp))
 
-          (container
-            style: (properties
-                     text-align: "center")
-            (br)
-            (h1 "Sign up for courses at Gallaudet!")
-            (br))
-            
-          (div
-           style: (properties
-                   position: "absolute"
-                   left: "50%"
-                   margin-left: -600)
-            (br)
-            (courses)
-            (point-of-sale))
+          #;(location-courses
+           "LOCATION 1"
+           (summer-camp)
+           (weekend-course))
 
+          #;(location-courses
+           "LOCATION 2"
+           (weekend-course)))
+          
           )))
 
 (define (pages)
   (list
     (bootstrap-files)
     (index)))
+
+
