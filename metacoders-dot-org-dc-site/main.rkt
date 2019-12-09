95cb9cc2e3813f850df293f19b933721d2922b61
aBlender <JasonLongLe@gmail.com>
2019-12-09 15:08:37 -0800

    Using new course-struct

--- metacoders-dot-org-dc-site/main.rkt
+++ metacoders-dot-org-dc-site/main.rkt
@@ -5,12 +5,11 @@
 (require metacoders-dot-org-dc-lib
          metacoders-dot-org-lib)
 
-(define (weekend-course-k2) ;TODO: Use a course struct
-  (course-card
-      #:title         "WEEKEND CLASS"
+(define (weekend-course-clicker-k2) ;TODO: Use a course struct
+  (course
       #:topic         "Point & Click Games!"
       #:description   "In our Winter program, students at Gallaudet will learn how to code their own whack-a-mole style games that utilize the mouse. Students will learn how to customize their game with near endless possible combinations: Are you a UFO collecting cows? Or a kid eating up their favorite foods and avoiding their least favorites? Or a ninja nabbing fruit and not the bombs? Their games will feature a point system, and students will code in-game items such that they gain or lose points when the player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!"
-      #:age-range     "K - 2nd"
+      #:grade-range     "K - 2nd"
       #:image-url     "https://s3-us-west-1.amazonaws.com/ts-email-assets-and-stuff/DSC_0603_370_200.jpg"
       #:meeting-dates (list "2/15/2020" "2/22/2020" "2/29/2020" "3/7/2020" "3/14/2020" "3/21/2020" "3/28/2020")
       #:start-time    "11:00 am"
@@ -20,7 +19,41 @@
       #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
       #:price         185
       #:sku           "sku_GG41ELVKDJ6PZc"
-      #:key           "pk_test_Jd6aRCVssUu8YfSvltaT3tvU00je9fQbkA"
+      #:status        'open
+   ))
+
+(define (weekend-course-clicker-k2-2)
+  (course
+      #:topic         "Point & Click Games!"
+      #:description   "In our Winter program, students at Gallaudet will learn how to code their own whack-a-mole style games that utilize the mouse. Students will learn how to customize their game with near endless possible combinations: Are you a UFO collecting cows? Or a kid eating up their favorite foods and avoiding their least favorites? Or a ninja nabbing fruit and not the bombs? Their games will feature a point system, and students will code in-game items such that they gain or lose points when the player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!"
+      #:grade-range     "K - 2nd"
+      #:image-url     "https://s3-us-west-1.amazonaws.com/ts-email-assets-and-stuff/DSC_0603_370_200.jpg"
+      #:meeting-dates (list "2/15/2020" "2/22/2020" "2/29/2020" "3/7/2020" "3/14/2020" "3/21/2020" "3/28/2020")
+      #:start-time    "11:00 am"
+      #:end-time      "12:30 pm"
+      #:location      "Gallaudet University"
+      #:address       "800 Florida Ave NE, Washington, DC 20002"
+      #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
+      #:price         185
+      #:sku           "fake_course_sku_2"
+      #:status        'almost-full
+   ))
+
+(define (weekend-course-clicker-k2-3)
+  (course
+      #:topic         "Point & Click Games!"
+      #:description   "In our Winter program, students at Gallaudet will learn how to code their own whack-a-mole style games that utilize the mouse. Students will learn how to customize their game with near endless possible combinations: Are you a UFO collecting cows? Or a kid eating up their favorite foods and avoiding their least favorites? Or a ninja nabbing fruit and not the bombs? Their games will feature a point system, and students will code in-game items such that they gain or lose points when the player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!"
+      #:grade-range     "K - 2nd"
+      #:image-url     "https://s3-us-west-1.amazonaws.com/ts-email-assets-and-stuff/DSC_0603_370_200.jpg"
+      #:meeting-dates (list "2/15/2020" "2/22/2020" "2/29/2020" "3/7/2020" "3/14/2020" "3/21/2020" "3/28/2020")
+      #:start-time    "11:00 am"
+      #:end-time      "12:30 pm"
+      #:location      "Gallaudet University"
+      #:address       "800 Florida Ave NE, Washington, DC 20002"
+      #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
+      #:price         185
+      #:sku           "fake_course_sku_3"
+      #:status        'full
    ))
 
 (define (summer-camp-clicker-k-2nd-7/15)
@@ -33,7 +66,7 @@
                             "Their games will feature a point system, and students will code in-game items such that they gain or lose points when the "
                             "player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. "
                             "Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!")
-        #:age-range     "Entering K - 2nd"
+        #:grade-range     "Entering K - 2nd"
         #:location      "Gallaudet University"
         #:address       "800 Florida Ave NE, Washington, DC 20002"
         #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
@@ -43,7 +76,7 @@
         #:lunch-time    "12:00pm - 1:00pm"
         #:pickup-time   "1:00pm - 1:15pm"
         #:meeting-dates (list "7/15/2020" "7/16/2020" "7/17/2020" "7/18/2020" "7/19/2020")
-        #:open?         #t
+        #:status         'open
         ))
 
 (define (summer-camp-animal-k-2nd-7/15)
@@ -56,7 +89,7 @@
                             "Their games will feature a point system, and students will code in-game items such that they gain or lose points when the "
                             "player clicks on them. This course will not only strengthen students’ coding skills, but also their keyboard and mouse skills. "
                             "Throughout the course, students will earn physical badges that demonstrate their mastery and inspire them to keep coding!")
-        #:age-range     "Entering K - 2nd"
+        #:grade-range     "Entering K - 2nd"
         #:location      "Gallaudet University"
         #:address       "800 Florida Ave NE, Washington, DC 20002"
         #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
@@ -66,7 +99,7 @@
         #:lunch-time    "12:00pm - 1:00pm"
         #:pickup-time   "1:00pm - 1:15pm"
         #:meeting-dates (list "7/15/2020" "7/16/2020" "7/17/2020" "7/18/2020" "7/19/2020")
-        #:open?         #f
+        #:status         'full
         ))
 
 (define (summer-camp-hp-3rd-6th-7/15)
@@ -77,7 +110,7 @@
                             "a point of inspiration, we will teach students how to craft their own adventure game inside a unique wizarding world. In addition "
                             "to learning how to code, students will also be guided in the ways of digital citizenship and how to appropriately and safely "
                             "share their fan-fictional work with the world. Students are going to love coding their own wizarding world full of mystery & magic!")
-        #:age-range     "Entering 3rd - 6th"
+        #:grade-range     "Entering 3rd - 6th"
         #:location      "Gallaudet University"
         #:address       "800 Florida Ave NE, Washington, DC 20002"
         #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
@@ -86,7 +119,7 @@
         #:camp-time     "1:00pm - 4:00pm"
         #:pickup-time   "4:00pm - 4:15pm"
         #:meeting-dates (list "7/15/2020" "7/16/2020" "7/17/2020" "7/18/2020" "7/19/2020")
-        #:open?         #f
+        #:status         'full
         ))
 
 (define (summer-camp-fortnite-3rd-6th-7/15)
@@ -97,7 +130,7 @@
                             "a point of inspiration, we will teach students how to craft their own adventure game inside a unique wizarding world. In addition "
                             "to learning how to code, students will also be guided in the ways of digital citizenship and how to appropriately and safely "
                             "share their fan-fictional work with the world. Students are going to love coding their own wizarding world full of mystery & magic!")
-        #:age-range     "Entering 3rd - 6th"
+        #:grade-range     "Entering 3rd - 6th"
         #:location      "Gallaudet University"
         #:address       "800 Florida Ave NE, Washington, DC 20002"
         #:address-link  "https://goo.gl/maps/Yypsi9LRZB7sFTjc9"
@@ -106,7 +139,7 @@
         #:camp-time     "1:00pm - 4:00pm"
         #:pickup-time   "4:00pm - 4:15pm"
         #:meeting-dates (list "7/15/2020" "7/16/2020" "7/17/2020" "7/18/2020" "7/19/2020")
-        #:open?         #f
+        #:status         'almost-full
         ))
 
 (define (index)
@@ -114,7 +147,9 @@
         (city-page
          #:city-name  "Washington, D.C."
          #:banner-url "https://secularstudents.org/wp-content/uploads/2019/09/US-Capitol.jpeg"
-         #:school-year-courses (school-year-courses (weekend-course-k2))
+         #:school-year-courses (list (weekend-course-clicker-k2)
+                                     (weekend-course-clicker-k2-2)
+                                     (weekend-course-clicker-k2-3))
          #:summer-camps (list (summer-camp-clicker-k-2nd-7/15)
                               (summer-camp-animal-k-2nd-7/15)
                               (summer-camp-hp-3rd-6th-7/15)
